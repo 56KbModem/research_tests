6d2946d9d74b0f51ed3ac82739a4b0baccf320fb
56KbModem <nick-snel@hotmail.com>
2020-07-06 13:49:54 +0200

    modified cv2 test

--- OpenCV_tests/test.py
+++ OpenCV_tests/test.py
@@ -2,17 +2,30 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture("http://192.168.1.5:8080")
+vid_stream = "http://192.168.1.5:8080"
+cap = cv2.VideoCapture(vid_stream)
+frame_count = 0
+
+if not cap.isOpened():
+    print("[!] cannot open stream\n[!] exiting...")
+    exit()
 
 while True:
-    while(cap.isOpened()):
-        ret, frame = cap.read()
+    ret, frame = cap.read()
+    frame_count += 1
+    if not ret:
+        print("[!] cannot receive frame (stream ended?)\n[!] exiting...")
+        break
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if frame_count == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+        frame_count = 0
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
 
-        cv2.imshow('webcam',gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cap.release()
-    cv2.destroyAllWindows()
+    cv2.imshow('webcam',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
