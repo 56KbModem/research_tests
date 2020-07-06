ca50e021d7c7b73a620326f8e67e9d38425a7cf1
Cris <crisvdnolle@hotmail.com>
2020-07-06 12:56:51 +0200

    keep opening stream

--- OpenCV_tests/test.py
+++ OpenCV_tests/test.py
@@ -3,14 +3,15 @@ import cv2
 
 cap = cv2.VideoCapture("http://192.168.1.5:8080")
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
+while True:
+    while(cap.isOpened()):
+        ret, frame = cap.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('webcam',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow('webcam',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
